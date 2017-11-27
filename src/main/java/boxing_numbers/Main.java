f1ca0ff335b9ae372f1f9973cf1b340183c2a42c
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-11-27 21:35:32 +0000

    Method arguments autoboxed

---
+++ src/main/java/boxing_numbers/Main.java
@@ -0,0 +1,17 @@
+package boxing_numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+
+        /** Autobox to Integer -> select most specific */
+        doit(i);
+    }
+    static void doit(Number i){
+        System.out.println("Correct");
+    }
+
+    static void(Object i){
+        System.out.println("Missed");
+    }
+}
