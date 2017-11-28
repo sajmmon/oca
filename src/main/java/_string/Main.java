b372adb04756f9c6e7d59b31c3b66af4af81e720
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-11-28 18:53:32 +0000

    _string

--- src/main/java/_string/Main.java
+++ src/main/java/_string/Main.java
@@ -3,6 +3,12 @@ package _string;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "abc".substring(2,3);
+        String s = "abc";
+        String s1 = s.substring(0,3);
+        String s2 = s.substring(0,3);
+
+        System.out.println(s1 == s2);
+
+        s.equals('c');
     }
 }
