3c5ff4bff9276af0c1c29758ffb12d639ef250d4
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-11-29 16:37:22 +0000

    Shadow lambda

--- src/main/java/_lambda/Main.java
+++ src/main/java/_lambda/Main.java
@@ -1,4 +1,22 @@
 package _lambda;
 
+import java.util.function.Predicate;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Predicate p;
+
+        /** Can access non-final variables*/
+        p = obj -> {
+            if (args.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        System.out.println(p);
+
+    }
 }
--- src/main/java/_lambda/MainNC.java
+++ src/main/java/_lambda/MainNC.java
@@ -1,4 +1,31 @@
 package _lambda;
 
+import java.util.function.Predicate;
+
 public class MainNC {
+    static int i = 0;
+
+    public static void main(String[] args) {
+        Predicate<String> p;
+        /** Must return true/false not optional*/
+        p = obj -> {
+            if (true) return true;
+        };
+
+
+        /** Can not shadow variables */
+        p = args -> true;
+
+
+        /** Can not also in this way */
+        p = obj -> {String args = null; return true;};
+
+
+
+
+
+
+
+
+    }
 }
