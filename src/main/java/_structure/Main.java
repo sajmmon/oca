e495f29df8b4f3c7fc94e385133b81a9692e2829
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-02 22:49:45 +0000

    Static import

---
+++ src/main/java/_structure/Main.java
@@ -0,0 +1,11 @@
+package _structure;
+
+import java.lang.Math;
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(PI);
+    }
+}
