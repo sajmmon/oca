9115551e20c4d589e1fdd8f23d13a60fffd5adb5
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-04 16:52:22 +0000

    Constructor

---
+++ src/main/java/_structure/MainNC.java
@@ -0,0 +1,19 @@
+package _structure;
+
+public class MainNC {
+
+    /** Allowed */
+    private MainNC(){}
+//    public MainNC(){}
+//    protected MainNC(){}
+
+    /** Now it is not constructor but method */
+    //void MainNC(){}
+
+    /** final, static, default not allowed, only access modifiers */
+//    final MainNC(){}
+//    static MainNC(){}
+//    default MainNC(){}
+
+
+}
