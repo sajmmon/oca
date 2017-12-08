8be92858fee1e1c573f11b38090da66ebbac6318
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-08 20:08:46 +0000

    Operator precedence

--- src/main/java/_operators/Main.java
+++ src/main/java/_operators/Main.java
@@ -1,5 +1,10 @@
 package _operators;
 
+import java.io.Serializable;
+import java.util.List;
+
+import static java.lang.Integer.MAX_VALUE;
+
 public class Main {
     public static void main(String[] args) {
         int i = 1;
@@ -20,6 +25,29 @@ public class Main {
         int u = (- 7) % -3;
         System.out.println(x + "\t" + y + "\t" + z + "\t" + w + "\t" + u);
 
+
+        /** Precenence
+         * () [] .
+         * ++ -- (post)
+         * ++ -- (pre) !
+         * new () cast
+         * * / %
+         * + - + string concatenation
+         * << >>
+         * < > instanceof
+         * != ==
+         * &&
+         * ||
+         * ?:
+         * = += -= *= /=
+         */
+
+        int x1 = 1;
+        System.out.println( x1++ + ++x1 * --x1 ); // 7
+        System.out.println("A" + 5 * 6 + "B"); // A30B
+        System.out.println(1 + + + + 2); // 3
+        System.out.println(1 + + + + 2 - - 3); // 6
+        System.out.println(1 + + + + 2 - - - 3); // 0
 
     }
 }
