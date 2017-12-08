0e9a73c36ad2ae5b79afc45c9c44f305b156d445
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-08 15:56:51 +0000

    date plus requires unit

--- src/main/java/_datetime/MainNC.java
+++ src/main/java/_datetime/MainNC.java
@@ -1,4 +1,15 @@
 package _datetime;
 
+import java.time.LocalDate;
+
 public class MainNC {
+    public static void main(String[] args) {
+        LocalDate ld = LocalDate.now();
+        // ld.plus(//amount, unit);
+        ld.plusDays(1);
+        ld.plusMonths(1);
+        ld.plusYears(1);
+        ld.plusWeeks(1);
+        // no plus(int)
+    }
 }
