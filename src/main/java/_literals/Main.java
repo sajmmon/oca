c51c7a4638e2ea35c47b536bcaa528d6ca2eaceb
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-08 20:14:27 +0000

    Literals

---
+++ src/main/java/_literals/Main.java
@@ -0,0 +1,12 @@
+package _literals;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        /** Octal literal */
+        int i3 = 012;
+        System.out.println(i3); // 10
+    }
+}
