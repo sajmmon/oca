38549d0373cb6508698685d7546f6061202e7ba0
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-09 21:04:26 +0000

    Change String tests

--- src/main/java/_string/Main.java
+++ src/main/java/_string/Main.java
@@ -3,6 +3,7 @@ package _string;
 public class Main {
 
     public static void main(String[] args) {
+
         String s = "abc";
         String s1 = s.substring(0,3);
         String s2 = s.substring(0,3);
