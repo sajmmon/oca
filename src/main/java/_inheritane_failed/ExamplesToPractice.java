31b559ac939d1260e0c60db73487a352a9496fa3
Szymon Chojnacki <chojnasm@ebi.ac.uk>
2017-12-19 10:26:25 +0000

    Problems with inheritance

---
+++ src/main/java/_inheritane_failed/ExamplesToPractice.java
@@ -0,0 +1,13 @@
+package _inheritane_failed;
+
+public class ExamplesToPractice {
+
+    /**
+     * Chapter 7
+     * 7, 9, 25, 27, 29, 37
+     * static and non-static can not override each other
+     */
+    public static void main(String[] args) {
+
+    }
+}
